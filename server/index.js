b1d358e0b80fbd74b2c5b035cb6f25ac872ff642
pugalkmc <pugalkmc@gmail.com>
2024-03-16 23:49:17 +0530

    vercel serverless setup

--- server/index.js
+++ server/index.js
@@ -9,13 +9,8 @@ const app = express();
 
 const router = express.Router();
 app.use(express.json());
-const allowedOrigins = ['https://bidly-auction-project-delta.vercel.app'];
-app.use(cors({
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-  }))
+
+app.use(cors())
 
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`, req.body);
@@ -27,6 +22,8 @@ app.use('/auction', authenticateToken, auction);
 app.use('/user', authenticateToken, userRouter)
 app.post('/newsletter/subscribe', authenticateToken, (req, res) => subscribeEmail(req, res));
 
-app.listen(3001, () => {
-    console.log(`Server running on PORT: ${3001}`);
-});
+// app.listen(3001, () => {
+//     console.log(`Server running on PORT: ${3001}`);
+// });
+
+export default app;
